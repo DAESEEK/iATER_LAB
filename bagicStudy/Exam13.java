58780bb8d7efe724f111eaf42ccc6de088efd51c
baek.sungduk <baek.sungduk@nhnpayco.com>
2021-10-23 22:03:00 +0900

    Add Diamond

---
+++ bagicStudy/Exam13.java
@@ -0,0 +1,40 @@
+package bagicStudy;
+
+public class Exam13 {
+
+	public static void main(String[] args) {
+		// ???? ???? ??? ??
+		int size = 5;
+		// ????? ??
+		Diamond diamond = new Diamond(size);
+		// ????? ??
+		diamond.print();
+
+
+//		for (int i = 0; i <= 6; i++) {
+//			for (int j = 6 - i; j > 0; j--) {
+//				System.out.print(" ");
+//			}
+//			for (int k = 1; k <= i; k++) {
+//				System.out.print("*");
+//			}
+///* */
+//			for (int l = 0; l < i - 1; l++) {
+//				System.out.print("*");
+//			}
+//			System.out.println(" ");
+//		}
+//		for (int i = 0; i <= 5; i++) {
+//			for (int j = 0; j <= i; j++) {
+//				System.out.print(" ");
+//			}
+//			for (int k = 4; k >= i; k--) {
+//				System.out.print("*");
+//			}
+//			for (int l = 0; l < 4-i; l++) {
+//				System.out.print("*");
+//			}
+//			System.out.println(" ");
+//		}
+	}
+}
